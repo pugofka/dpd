0fb05e05d017ded085a51ef1bf9f645e325a4582
Yushkevich Vitaly <yushkevichv@gmail.com>
2017-09-08 15:59:39 +0300

    change config

--- config/dpd.php
+++ config/dpd.php
@@ -2,27 +2,15 @@
 
 return [
     /*
-     * The view id of which you want to display data.
+     * client number from DPD
      */
-    'view_id' => env('ANALYTICS_VIEW_ID'),
+    'number' => env('DPD_ID'),
     /*
-     * Path to the client secret json file. Take a look at the README of this package
-     * to learn how to get this file.
+     * integration api key. You can get it from you account
      */
-    'service_account_credentials_json' => storage_path('app/analytics/service-account-credentials.json'),
+    'key' => env('DPD_KEY'),
     /*
-     * The amount of minutes the Google API responses will be cached.
-     * If you set this to zero, the responses won't be cached at all.
+     * test mode. Default enabled
      */
-    'cache_lifetime_in_minutes' => 60 * 24,
-    /*
-     * Here you may configure the "store" that the underlying Google_Client will
-     * use to store it's data.  You may also add extra parameters that will
-     * be passed on setCacheConfig (see docs for google-api-php-client).
-     *
-     * Optional parameters: "lifetime", "prefix"
-     */
-    'cache' => [
-        'store' => 'file',
-    ],
+    'test_mode' => env('DPD_TEST_MODE'),
 ];
