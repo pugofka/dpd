e15214cb71416fbfd806dfa715f6ace261e1be65
Yushkevich Vitaly <yushkevichv@gmail.com>
2017-09-11 17:20:43 +0300

    base cost calculate

--- src/Dpd.php
+++ src/Dpd.php
@@ -69,5 +69,56 @@ class Dpd
 
     }
 
+    public function getCostByParametres(
+            string $from,
+            string $to,
+            bool $selfPickup = true,
+            bool $selfDelivery=false,
+            float $weight = 0,
+            float $declaredValue = 0,
+            float $volume = null,
+            $pickupDate = null
+        )
+    {
+        $client = new \SoapClient($this->client->url."calculator2?wsdl",
+            [
+                'trace' => true,
+                'keep_alive' => false
+            ]
+        );
+
+        $data['auth'] = $this->client->getAuthData();
+        $data['selfDelivery'] = $selfDelivery;
+        $data['selfDelivery'] = $selfDelivery;
+        $data['selfPickup'] = $selfPickup;
+        $data['weight'] = $weight;
+//        $data['declaredValue'] = $declaredValue;
+        $cityFrom = $this->findCity($from);
+        $data['pickup'] = [
+            'cityId' => $cityFrom->cityId,
+            'cityName'  => $cityFrom->cityName,
+        ];
+        $cityTo = $this->findCity($to);
+        $data['delivery' ] = [
+            'cityId' => $cityTo->cityId,
+            'cityName'  => $cityTo->cityName,
+        ];
+
+//        @todo need to refactor
+        $data['parcel'] = [
+            'weight' => 1,
+            'length' => 1,
+            'width' => 1,
+            'height' => 1 ,
+            'quantity' => 1
+        ];
+
+
+        $request['request'] = $data; //помещаем наш масив авторизации в масив запроса request.
+        $result = $client->getServiceCostByParcels2($request); //обращаемся к функции getCitiesCashPay  и получаем список городов.
+        return $result = self::stdToArray($result);
+
+    }
+
 
 }
