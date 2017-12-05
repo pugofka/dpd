2a06e41b1265d7a7ff9b52ea643997ce101be916
Vyacheslav Skoropupov <v.skoropupov@pugofka.com>
2017-12-05 12:22:04 +0300

    add new common client with catch exceptions

--- src/DpdClient.php
+++ src/DpdClient.php
@@ -2,8 +2,6 @@
 
 namespace Pugofka\Dpd;
 
-use Illuminate\Contracts\Cache\Repository;
-
 class DpdClient
 {
     public $number;
@@ -12,17 +10,21 @@ class DpdClient
     public $cacheLifeTimeInMinutes;
     protected $testMode;
 
-    /** @var \Illuminate\Contracts\Cache\Repository */
-    protected $cache;
+    private $client;
 
     /** @var int */
 
     const API_URL_TEST = 'http://wstest.dpd.ru/services/';
     const API_URL_PROD = 'http://ws.dpd.ru/services/';
 
+    const apiMethods = [
+        'getCitiesCashPay' => 'geography',
+        'getParcelShops' => 'geography',
+        'getServiceCost2' => 'calculator',
+    ];
+
     public function __construct()
     {
-        $cache = app(Repository::class);
         $this->number = config('dpd.number');
         $this->key = config('dpd.key');
         $this->testMode = config('dpd.test_mode');
@@ -32,8 +34,6 @@ class DpdClient
             $this->url = self::API_URL_TEST;
         else
             $this->url = self::API_URL_PROD;
-
-        $this->cache = $cache;
     }
 
     public function getAuthData()
@@ -44,5 +44,75 @@ class DpdClient
         ];
     }
 
+    public function getConnection($path) {
+
+        if ($this->client) {
+            return $this->client;
+        }
+
+        try {
+
+            $client = new \SoapClient($this->url . $path . "2?wsdl",
+                [
+                    'trace' => true,
+                    'keep_alive' => false
+                ]
+            );
+
+            $this->client = $client;
+
+        } catch (\Exception $e) {
+
+            return $result['errorMessage'] = 'dpd service connection error';
+
+        }
+
+        return $client;
+    }
+
+    public function invoke($method, $data = [], $toArray = true) {
+        try {
+
+            $client = $this->getConnection(
+                self::apiMethods[$method]
+            );
+
+            $data['auth'] = $this->getAuthData();
+
+            $request['request'] = $data;
+
+            $result = $client->{$method}($request);
+
+            if ($toArray) {
+                $result = $this->_parceObj2Arr($result->return);
+            }
+
+            return $result;
+
+        } catch (\Exception $e) {
+
+            return $result['errorMessage'] = $e->getMessage();
+
+        }
+    }
+
+    private function _parceObj2Arr($obj, $isUTF = 1, $arr = array()) {
+        $isUTF = $isUTF ? 1 : 0;
+        if (is_object($obj) || is_array($obj)) {
+            $arr = array();
+            for (reset($obj); list($k, $v) = each($obj);) {
+                if ($k === "GLOBALS")
+                    continue;
+                $arr[$k] = $this->_parceObj2Arr($v, $isUTF, $arr);
+            }
+            return $arr;
+        } elseif (gettype($obj) == 'boolean') {
+            return $obj ? 'true' : 'false';
+        } else {
+//            if ($isUTF && gettype($obj) == 'string')
+//                $obj = iconv('utf-8', 'windows-1251', $obj);
+            return $obj;
+        }
+    }
 
 }
